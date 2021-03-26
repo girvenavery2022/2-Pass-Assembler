50118bfb2fb0cf7ec2ec3aac17d3b09e23db3d5b
Avery <girvenavery@gmail.com>
2021-03-26 12:18:41 -0400

    Initial Commit

---
+++ Program 5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Program 5
+//
+//  Created by Avery Girven on 3/26/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
